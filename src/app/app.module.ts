10df08894e15466acff10198890822177d73ea12
Karol Bulwin <karolbulwin@gmail.com>
2019-07-25 23:05:06 +0200

    http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { CoreModule } from './core/core/core.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, NavComponent],
@@ -19,6 +20,7 @@ import { CoreModule } from './core/core/core.module';
     BrowserModule,
     AppRoutingModule,
     CoreModule,
+    HttpClientModule,
     // material
     BrowserAnimationsModule,
     LayoutModule,
