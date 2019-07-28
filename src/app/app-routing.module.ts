13a7b56a4737d6c184d79f0a3eb3c0630904dc8b
Karol Bulwin <karolbulwin@gmail.com>
2019-07-28 21:43:03 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AddBookComponent } from './books/add-book/add-book.component';
+import { AddReaderComponent } from './readers/add-reader/add-reader.component';
 
-const routes: Routes = [{ path: '', component: DashboardComponent }];
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'addBook', component: AddBookComponent },
+  { path: 'addReader', component: AddReaderComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
