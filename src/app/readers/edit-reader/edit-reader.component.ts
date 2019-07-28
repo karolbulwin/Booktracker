7bd7d521b596a8d7ca7b5bceca597a336067ca6f
Karol Bulwin <karolbulwin@gmail.com>
2019-07-28 21:47:19 +0200

    edit reader

---
+++ src/app/readers/edit-reader/edit-reader.component.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/core/data.service';
+import { Reader } from 'src/app/models/reader';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { Validators, FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-edit-reader',
+  templateUrl: './edit-reader.component.html',
+  styleUrls: ['./edit-reader.component.scss']
+})
+export class EditReaderComponent implements OnInit {
+  readerForm;
+  selectedReader: Reader;
+
+  constructor(
+    private route: ActivatedRoute,
+    private fb: FormBuilder,
+    private dataService: DataService,
+    private location: Location
+  ) {}
+
+  // getErrorMessage() {
+  //   return this.year.hasError('required')
+  //     ? 'You must enter a value'
+  //     : this.year.hasError('email')
+  //     ? 'Not a valid email'
+  //     : '';
+  // }
+
+  getReader(): void {
+    const readerID = +this.route.snapshot.params.id;
+
+    this.dataService.getReader(readerID).subscribe(
+      (reader: Reader) => {
+        this.selectedReader = reader;
+        this.readerForm = this.fb.group({
+          name: [this.selectedReader.name, Validators.required],
+          weeklyReadingGoal: [
+            this.selectedReader.weeklyReadingGoal,
+            Validators.required
+          ],
+          totalMinutesRead: [
+            this.selectedReader.totalMinutesRead,
+            Validators.required
+          ]
+        });
+      },
+      (err: any) => console.log('edit-reader: ' + err) // in dataService
+    );
+  }
+
+  onSubmit(): void {
+    console.warn('Saved');
+
+    if (this.readerForm.status === 'VALID') {
+      const updatedReader: Reader = this.readerForm.value as Reader;
+      const newReader = { ...this.selectedReader, ...updatedReader };
+
+      this.dataService
+        .updateReader(newReader)
+        .subscribe(
+          (data: void) =>
+            console.log(`${this.selectedReader.name} updated successfully`),
+          (err: any) => console.log(err),
+          () => this.goBack()
+        );
+    }
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  ngOnInit(): void {
+    this.getReader();
+  }
+}
