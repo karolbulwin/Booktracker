391725e791a1b5da221c6dcee732617695171e06
Karol Bulwin <karolbulwin@gmail.com>
2019-07-29 16:08:32 +0200

    removed unused code

--- src/app/readers/add-reader/add-reader.component.ts
+++ src/app/readers/add-reader/add-reader.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from 'src/app/core/data.service';
 import { Reader } from 'src/app/models/reader';
-// import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { Validators, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -21,28 +20,15 @@ export class AddReaderComponent implements OnInit {
   error: string;
 
   constructor(
-    // private route: ActivatedRoute,
     private fb: FormBuilder,
     private dataService: DataService,
     private location: Location,
     private router: Router
   ) {}
 
-  // getErrorMessage() {
-  //   return this.year.hasError('required')
-  //     ? 'You must enter a value'
-  //     : this.year.hasError('email')
-  //     ? 'Not a valid email'
-  //     : '';
-  // }
-
   onSubmit(): void {
     if (this.readerForm.status === 'VALID') {
       const newReader: Reader = this.readerForm.value as Reader;
-      // newReader.id = 0;
-
-      // console.log(newReader);
-      // console.log(this.readerForm.value);
 
       this.dataService.addReader(newReader).subscribe(
         (reader: Reader) => console.log(reader),
