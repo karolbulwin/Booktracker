ff4c99b41aff377c1fc2f6a74c33716a99aa143f
Karol Bulwin <karolbulwin@gmail.com>
2019-07-29 16:04:22 +0200

    remove unused code

--- src/app/books/edit-book/edit-book.component.ts
+++ src/app/books/edit-book/edit-book.component.ts
@@ -3,7 +3,7 @@ import { DataService } from 'src/app/core/data.service';
 import { Book } from 'src/app/models/book';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-import { FormControl, Validators, FormBuilder } from '@angular/forms';
+import { Validators, FormBuilder } from '@angular/forms';
 import { BookTrackerError } from 'src/app/models/bookTrackerError';
 
 @Component({
@@ -12,13 +12,6 @@ import { BookTrackerError } from 'src/app/models/bookTrackerError';
   styleUrls: ['./edit-book.component.scss']
 })
 export class EditBookComponent implements OnInit {
-  // bookForm = this.fb.group({
-  //   title: [this.selectedBook.title, Validators.required],
-  //   author: [this.selectedBook.author, Validators.required],
-  //   publicationYear: [null, Validators.required]
-  // });
-
-  //   new FormControl('', [Validators.required, Validators.email]);
   bookForm;
   selectedBook: Book;
   error = {
@@ -32,14 +25,6 @@ export class EditBookComponent implements OnInit {
     private dataService: DataService,
     private location: Location
   ) {}
-
-  // getErrorMessage() {
-  //   return this.year.hasError('required')
-  //     ? 'You must enter a value'
-  //     : this.year.hasError('email')
-  //     ? 'Not a valid email'
-  //     : '';
-  // }
 
   getBook(): void {
     const bookID = +this.route.snapshot.params.id;
@@ -63,15 +48,11 @@ export class EditBookComponent implements OnInit {
   }
 
   onSubmit(): void {
-    console.warn('Saved');
-
     if (this.bookForm.status === 'VALID') {
       const updatedBook: Book = this.bookForm.value as Book;
       const newBook = { ...this.selectedBook, ...updatedBook };
 
       this.dataService.updateBook(newBook).subscribe(
-        (data: void) =>
-          console.log(`${this.selectedBook.title} updated successfully`),
         (error: BookTrackerError) => {
           // this.error = error.friendlyMessage as string;
           this.error.send = 'An error occurred. Please try again later.';
