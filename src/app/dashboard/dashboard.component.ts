d6af7601fbd042195a04464f4bba1dc5ab37c468
Karol Bulwin <karolbulwin@gmail.com>
2019-07-29 12:42:25 +0200

    refactored

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { map, tap, flatMap } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
 import { Book } from '../models/book';
 import { DataService } from '../core/data.service';
 import { Reader } from '../models/reader';
+import { BookTrackerError } from '../models/bookTrackerError';
 
 @Component({
   selector: 'app-dashboard',
@@ -14,25 +14,36 @@ import { Reader } from '../models/reader';
 export class DashboardComponent implements OnInit {
   books: Book[];
   readers: Reader[];
-  cards: Observable<object[]>;
+  errors = {
+    books: '',
+    readers: ''
+  };
 
-  smallView(books: Book[], readers: Reader[]) {
+  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(({ matches }) => {
+      if (matches) {
+        return this.smallView();
+      }
+      return this.normalView();
+    })
+  );
+
+  smallView() {
     return [
-      { title: 'Books', cols: 2, rows: 1, data: books },
+      { title: 'Books', cols: 2, rows: 1 },
       {
         title: 'Readers',
         cols: 2,
-        rows: 1,
-        data: readers
+        rows: 1
       },
       { title: 'Card 3', cols: 2, rows: 1 }
     ];
   }
 
-  normalView(books: Book[], readers: Reader[]) {
+  normalView() {
     return [
-      { title: 'Books', cols: 2, rows: 1, data: books },
-      { title: 'Readers', cols: 1, rows: 1, data: readers },
+      { title: 'Books', cols: 2, rows: 1 },
+      { title: 'Readers', cols: 1, rows: 1 },
       { title: 'Card 3', cols: 1, rows: 1 }
     ];
   }
@@ -45,30 +56,39 @@ export class DashboardComponent implements OnInit {
   ngOnInit(): void {
     this.dataService.getBooks().subscribe(
       (books: Book[]) => {
-        console.log(books);
         this.books = books;
-        this.dataService.getReaders().subscribe(
-          (readers: Reader[]) => {
-            console.log(readers);
-            this.readers = readers;
-
-            this.cards = this.breakpointObserver
-              .observe(Breakpoints.Handset)
-              .pipe(
-                map(({ matches }) => {
-                  if (matches) {
-                    return this.smallView(this.books, this.readers);
-                  }
-                  return this.normalView(this.books, this.readers);
-                })
-              );
-          },
-          (error: any) => console.log(error),
-          () => console.log('hmm')
-        );
       },
-      (error: any) => console.log(error),
-      () => console.log('Got them! or no?')
+      (error: BookTrackerError) => {
+        this.errors.books = error.friendlyMessage as string;
+      }
+    );
+    this.dataService.getReaders().subscribe(
+      (readers: Reader[]) => {
+        this.readers = readers;
+      },
+      (error: BookTrackerError) => {
+        this.errors.readers = error.friendlyMessage as string;
+      }
+    );
+  }
+
+  deleteBook(bookID: number): void {
+    this.dataService.deleteBook(bookID).subscribe(
+      (data: void) => {
+        this.books = this.books.filter(book => book.id !== bookID);
+        console.log(this.books);
+      },
+      (err: any) => console.log(err)
+    );
+  }
+
+  deleteReader(readerID: number): void {
+    this.dataService.deleteReader(readerID).subscribe(
+      (data: void) => {
+        this.readers = this.readers.filter(reader => reader.id !== readerID);
+        console.log(this.readers);
+      },
+      (err: any) => console.log(err)
     );
   }
 }
