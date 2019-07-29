43dc63beeeef8b474e2878fe85493a03c39b1796
Karol Bulwin <karolbulwin@gmail.com>
2019-07-29 11:45:43 +0200

    change to id

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -10,37 +10,37 @@ export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const books: Book[] = [
       {
-        bookID: 1,
+        id: 1,
         title: 'Goodnight Moon',
         author: 'Margaret Wise Brown',
         publicationYear: 1953
       },
       {
-        bookID: 2,
+        id: 2,
         title: 'Winnie-the-Pooh',
         author: 'A. A. Milne',
         publicationYear: 1926
       },
       {
-        bookID: 3,
+        id: 3,
         title: 'Where the Wild Things Are',
         author: 'Maurice Sendak',
         publicationYear: 1963
       },
       {
-        bookID: 4,
+        id: 4,
         title: 'The Hobbit',
         author: 'J. R. R. Tolkien',
         publicationYear: 1937
       },
       {
-        bookID: 5,
+        id: 5,
         title: 'Curious George',
         author: 'H. A. Rey',
         publicationYear: 1941
       },
       {
-        bookID: 6,
+        id: 6,
         title: "Alice's Adventures in Wonderland",
         author: 'Lewis Carroll',
         publicationYear: 1865
@@ -49,19 +49,19 @@ export class InMemoryDataService implements InMemoryDbService {
 
     const readers: Reader[] = [
       {
-        readerID: 1,
+        id: 1,
         name: 'Marie',
         weeklyReadingGoal: 400,
         totalMinutesRead: 5600
       },
       {
-        readerID: 2,
+        id: 2,
         name: 'Daniel',
         weeklyReadingGoal: 210,
         totalMinutesRead: 3000
       },
       {
-        readerID: 3,
+        id: 3,
         name: 'Lanier',
         weeklyReadingGoal: 140,
         totalMinutesRead: 600
