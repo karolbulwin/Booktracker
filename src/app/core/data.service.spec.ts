97fc0d46d13bdfdf0a39fd7b107b4e5fd8e3d2ba
Karol Bulwin <karolbulwin@gmail.com>
2019-07-30 11:58:38 +0200

    tests

--- src/app/core/data.service.spec.ts
+++ src/app/core/data.service.spec.ts
@@ -1,12 +1,45 @@
 import { TestBed } from '@angular/core/testing';
-
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+  TestRequest
+} from '@angular/common/http/testing';
 import { DataService } from './data.service';
+import { Book } from '../models/book';
+import { books } from '../data';
 
 describe('DataService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let dataServive: DataService;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [DataService]
+    });
+
+    dataServive = TestBed.get(DataService);
+    httpTestingController = TestBed.get(HttpTestingController);
+  });
 
   it('should be created', () => {
     const service: DataService = TestBed.get(DataService);
     expect(service).toBeTruthy();
+  });
+
+  it('should GET all books', () => {
+    dataServive.getBooks().subscribe((data: Book[]) => {
+      expect(data.length).toBe(6);
+    });
+
+    const booksRequest: TestRequest = httpTestingController.expectOne(
+      'https://my-json-server.typicode.com/karolbulwin/booktracker_db/books'
+      // '/api/books'
+    );
+    expect(booksRequest.request.method).toBe('GET');
+
+    booksRequest.flush(books);
+
+    httpTestingController.verify();
   });
 });
